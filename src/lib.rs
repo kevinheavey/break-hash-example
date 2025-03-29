1b3540ccbfa3dae103c1499acc3369948860ba8a
Kevin Heavey <heaveykevin2@gmail.com>
2025-03-29 11:15:48 +0000

    v1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,11 @@
-#[derive(Hash, Eq, PartialEq, Debug)]
+use std::hash::{Hash, Hasher};
+
+#[derive(Eq, PartialEq, Debug)]
 pub struct MyType(pub i32);
+
+impl Hash for MyType {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        // New hash: reverse the value
+        (!self.0).hash(state);
+    }
+}
